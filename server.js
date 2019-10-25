901a1b574299fdc8d8a872b654855fe1b55b0e12
JHaarla <haarladesign@gmail.com>
2019-10-24 23:50:42 -0400

    setup the server.js file - get express going

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
 const express = require("express");
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+// ser
+app.use(express.static("public"));
+
+// Parse application body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
