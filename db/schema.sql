386fd2b319bda393406bc842023c097742c83fd2
JHaarla <haarladesign@gmail.com>
2019-10-25 00:05:35 -0400

    added db schema and seeds

---
+++ db/schema.sql
@@ -0,0 +1,12 @@
+### schema
+
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+    id INT NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR(128) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (id)
+);
--- server.js
+++ server.js
@@ -10,3 +10,9 @@ app.use(express.static("public"));
 // Parse application body as JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+
+//handlebars setup
+const exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
