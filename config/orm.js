c0271e121376bf4acfca1e61834220a65aac7441
JHaarla <haarladesign@gmail.com>
2019-10-26 22:21:53 -0400

    logic done

--- config/orm.js
+++ config/orm.js
@@ -12,8 +12,31 @@ const orm = {
             }
             cb(result);
         });
-    }
-},
+    },
 
-//add burger
-insertOne: function(insert, )
+
+    //add burger
+    insertOne: function (insert, cb) {
+        const dbQuery = "INSERT INTO burgers SET ?";
+        connection.query(dbQuery, [insert], function (err, result) {
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    },
+
+    //change devoured status
+    updateOne: function (devoured, id, cb) {
+        const dbQuery = "UPDATE burgers SET ? WHERE id = ?";
+        connection.query(dbQuery, [devoured, id], function(err, result){
+            if (err) {
+                throw err;
+            }
+            cb(result);
+        });
+    }
+
+};
+
+module.exports = orm;
