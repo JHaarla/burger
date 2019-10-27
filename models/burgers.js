39599002876a8b9f36f70f36da857fef861a832c
JHaarla <haarladesign@gmail.com>
2019-10-26 22:33:13 -0400

    added burger.js model

--- models/burgers.js
+++ models/burgers.js
@@ -1,5 +1,20 @@
 const orm = require("../config/orm");
 
 const burger = {
-    selectAll: function (cb)
-}
+    selectAll: function (cb) {
+        orm.selectAll("burgers", function (result) {
+            cb(result);
+        });
+    },
+
+    insertOne: function (insert, cb) {
+        orm.insertOne(insert, cb);
+    },
+
+    updateOne: function (devoured, id, cb) {
+        orm.updateOne(devoured, id, cb);
+    }
+
+};
+
+module.exports = burger;
